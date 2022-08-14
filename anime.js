e252efd82ded9beda08461ea4b9852b895272a05
Z-Kamili <zakariakamili97@gmail.com>
2022-08-14 15:54:30 +0100

    updatedom

--- anime.js
+++ anime.js
@@ -1,20 +1,22 @@
-const base_url = "https://api.jikan.moe/v3/anime/1";
+const base_url = "https://api.jikan.moe/v3";
 
  function searchAnime(event){
 
     event.preventDefault();
-
-
-     const form = new FormData(this);
-     const query = form.get("search");
-
+    const form = new FormData(this);
+    const query = form.get("search");
      console.log(query);
+     fetch(`${base_url}/search/anime?q=${query}&page=1`)
+     .then(res => res.json())
+     .then(updateDom)
+     .cattch(err=>console.warn(err.message));
 
-     /*
-     fetch(base_url)
-    .then(res=>res.json())
-    .then(data=>console.log(data))
-    .catch(err=>console.warn(err.message));*/
+}
+
+function updateDom(data)
+{
+
+    data.results.forEach(anime=>console.log(anime));
 
 }
 
